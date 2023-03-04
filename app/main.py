e60420e29e489b1d67dd81d5923b39dda0827cff
PerfectMach1ne <75753483+PerfectMach1ne@users.noreply.github.com>
2023-03-04 14:16:19 +0100

    Finished 2 sections on DBs

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
-from typing import Optional
-from random import randrange
 
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
+from random import randrange
+import time
 
 app = FastAPI()  # FastAPI instance to call in uvicorn in order to start a server
+
+
 # command to type to start the server:
 # uvicorn app.main:app --reload
 
@@ -13,8 +18,24 @@ class Post(BaseModel):  # from Pydantic
     title: str
     content: str
     published: bool = True  # default value
-    rating: Optional[int] = None  # optional field
 
+
+while True:
+    try:
+        # Connect to an existing database
+        conn = psycopg2.connect(host='localhost', database='fastapi',
+                                user='postgres', password='chronium333',
+                                cursor_factory=RealDictCursor)
+        # cursor_factory thing in order to get access to column names
+
+        # Open a cursor to perform database operations
+        cursor = conn.cursor()
+        print("Database connection was successful!")
+        break
+    except Exception as err:
+        print("Database connection failed.")
+        print("Error: ", err)
+        time.sleep(5)
 
 my_posts = [{"id": 1, "title": "post title", "content": "example post content"},
             {"id": 2, "title": "favourite foods", "content": "piza..."}]
@@ -39,20 +60,37 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": my_posts}
+    cursor.execute("""SELECT * FROM posts; """)
+    posts = cursor.fetchall()
+    print(posts)
+    return {"data": posts}
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
-    post_dict = post.dict()
-    post_dict['id'] = randrange(0, 575757)  # bruh
-    my_posts.append(post_dict)
-    return {"data": post_dict}
+    cursor.execute("""INSERT INTO posts (title, content, published)
+                      VALUES (%s, %s, %s) RETURNING *; """,  # %s for data sanitization
+                   (post.title, post.content, post.published))
+    # Doing the same thing as with above but with f-strings makes you vulnerable to SQL injection attacks
+    # ...which is why passing data in there directly SUCKS and you should NEVER do that, since all our cool and awesome
+    # SQL-Python integration libraries can do SQL sanitization for our lazy, tired and confused asses.
+    new_post = cursor.fetchone()
+
+    conn.commit()  # Because we're making a change to the database.
+
+    return {"data": new_post}
 
 
 @app.get("/posts/{id}")
-def get_post(id: int):
-    post = find_post(int(id))
+def get_post(id: int):  # Issue: Internal Server Error 500 if nonexistent post id > 9
+    cursor.execute("""SELECT * FROM posts WHERE id = %s; """,
+                   (str(id),))
+    # YT guy just said this weird comma fixes the aforementioned issue.
+    # ...and well, it actually does. And they say that mathematics and quantum mechanics are
+    # the "dark magic" of STEM. Naw.
+    post = cursor.fetchone()
+    # print(post)
+
     if not post:  # If post is not found
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Post with the ID {id} was not found.")
@@ -62,22 +100,28 @@ def get_post(id: int):
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
-    index = find_index_post(id)
-    if index is None:
+    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *; """,
+                   (str(id),))
+    deleted_post = cursor.fetchone()
+    conn.commit()  # Because we're making a change to the database.
+
+    if deleted_post is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Post with the ID {id} does not exist.")
-    my_posts.pop(index)
+
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 @app.put("/posts/{id}")
 def update_post(id: int, post: Post):
-    index = find_index_post(id)
-    if index is None:
+    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s
+                      WHERE id = %s  RETURNING *; """,
+                   (post.title, post.content, post.published, str(id)))
+    updated_post = cursor.fetchone()
+    conn.commit()  # Because we're making a change to the database.
+
+    if updated_post is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Post with the ID {id} does not exist.")
-    post_dict = post.dict()
-    post_dict['id'] = id
-    my_posts[index] = post_dict
 
-    return {'data': post_dict}
+    return {'data': updated_post}
